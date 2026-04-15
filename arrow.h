b82dc8974b82af31ef9893982575a3d4662d82fd
Вика Бондаренко <bondarenkovika.151204@gmail.com>
2026-04-15 20:57:04 +0300

    15/12

---
+++ arrow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QGraphicsLineItem>
+#include <QPolygonF>
+
+class VisualNode;
+
+class Arrow : public QGraphicsLineItem {
+public:
+    Arrow(VisualNode* start, VisualNode* end); // конструктор стрелки
+    void updatePosition(); // функция обновления координат стрелки после изменения координат узлов
+    VisualNode* startItem() const { return current_start; }
+    VisualNode* endItem() const { return current_end; }
+
+protected:
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override; // функция рисования стрелки (инструмент для рисования, параметры объекта рисования, параметры для виджета)
+    QRectF boundingRect() const override; // функция, задающая ограничения для стрелки
+
+private:
+    VisualNode* current_start;
+    VisualNode* current_end;
+    QPolygonF arrow_head;
+    QColor my_color = Qt::black;
+};
